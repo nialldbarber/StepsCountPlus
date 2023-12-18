e358090f1d4afd2f1553d5419d61b2469500794e
Niall Barber <niall.barber@olioex.com>
2023-12-18 16:37:14 +0000

    chore: sentry ios build issue fices

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import { RootNavigator } from "@/app/navigation";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import * as Sentry from "@sentry/react-native";
 import React from "react";
+import Config from "react-native-config";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 Sentry.init({
-	dsn: "https://c10fd0e3f6024f0c865ec4351f8429fa@o429369.ingest.sentry.io/5375836",
+	dsn: Config.SENTRY_DSN,
 	tracesSampleRate: __DEV__ ? 1.0 : 0.5,
 });
 
