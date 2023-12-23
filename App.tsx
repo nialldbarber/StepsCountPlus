cb7bccb8b076dceb4f40f828420d05e799870965
Niall Barber <niall.barber@olioex.com>
2023-12-23 16:30:17 +0000

    feat: set up push notifications

--- App.tsx
+++ App.tsx
@@ -4,11 +4,27 @@ import * as Sentry from "@sentry/react-native";
 import React from "react";
 import Config from "react-native-config";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { LogLevel, OneSignal } from "react-native-onesignal";
 import Toast from "react-native-toast-message";
 
 Sentry.init({
 	dsn: Config.SENTRY_DSN,
 	tracesSampleRate: __DEV__ ? 1.0 : 0.5,
+});
+
+// Remove this method to stop OneSignal Debugging
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+// OneSignal Initialization
+OneSignal.initialize("23b8ae15-8c35-4a1b-a240-2dddbb36498b");
+
+// requestPermission will show the native iOS or Android notification permission prompt.
+// We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+OneSignal.Notifications.requestPermission(true);
+
+// Method for listening for notification clicks
+OneSignal.Notifications.addEventListener("click", (event) => {
+	console.log("OneSignal: notification clicked:", event);
 });
 
 function App() {
