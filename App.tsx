085420cc9e9a773204bf2963c900a0f17a99fc32
Niall Barber <niall.barber@olioex.com>
2023-12-23 16:32:22 +0000

    chore: tidy up

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ Sentry.init({
 OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
 // OneSignal Initialization
-OneSignal.initialize("23b8ae15-8c35-4a1b-a240-2dddbb36498b");
+OneSignal.initialize(Config.ONE_SIGNAL_INIT as string);
 
 // requestPermission will show the native iOS or Android notification permission prompt.
 // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
