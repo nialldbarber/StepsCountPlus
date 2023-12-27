1192d981c4212592f9bc91873a48bb1da57ce0db
Niall Barber <niall.barber@olioex.com>
2023-12-27 20:09:39 +0000

    chore: add tests for DailyDistanceGoal component

---
+++ app/components/goals/distance.test.tsx
@@ -0,0 +1,28 @@
+import { renderWithNavigation } from "@/app/lib/tests";
+import { screen } from "@testing-library/react-native";
+import { DailyDistanceGoal } from "./distance";
+
+describe("DailyDistanceGoal", () => {
+	beforeEach(() => {
+		renderWithNavigation(<DailyDistanceGoal />);
+	});
+
+	test("renders the GoalsCard component", () => {
+		expect(screen.getByText(/distance/i)).toBeTruthy();
+	});
+
+	test("renders the correct distance goal", () => {
+		expect(screen.getByText("5 km")).toBeTruthy();
+	});
+
+	test("renders the correct units", () => {
+		expect(screen.getByText("km")).toBeTruthy();
+	});
+
+	test.only("renders the correct options", () => {
+		expect(screen.getByText("5 km")).toBeTruthy();
+		expect(screen.getByText("7 km")).toBeTruthy();
+		expect(screen.getByText("10 km")).toBeTruthy();
+		expect(screen.getByText("15 km")).toBeTruthy();
+	});
+});
