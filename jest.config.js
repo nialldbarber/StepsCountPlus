15e20c525f933b9fa53ee2b6b07cbe7b5814e447
Niall Barber <niall.barber@olioex.com>
2023-12-27 20:09:29 +0000

    chore: update transformIgnorePatterns in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
 	setupFilesAfterEnv: ["./jest-setup.ts", "./jest-global-mocks.js"],
 	transformIgnorePatterns: [
-		"node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-navigation|@react-navigation/.*|react-native-svg|react-native-reanimated|react-native-haptic-feedback|react-native-unistyles)/)",
+		"node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-navigation|@react-navigation/.*|react-native-svg|react-native-reanimated|react-native-haptic-feedback|react-native-unistyles|@gorhom/bottom-sheet)/)",
 	],
 };
