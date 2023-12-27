57df4e51fdddc4ab889fd94b0216bef8b4a8c1a9
Niall Barber <niall.barber@olioex.com>
2023-12-27 19:19:51 +0000

    chore: add jest-global-mocks.js to setupFilesAfterEnv in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	preset: "@testing-library/react-native",
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-	setupFilesAfterEnv: ["./jest-setup.ts"],
+	setupFilesAfterEnv: ["./jest-setup.ts", "./jest-global-mocks.js"],
 	transformIgnorePatterns: [
-		"node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-navigation|@react-navigation/.*|react-native-svg|react-native-reanimated)/)",
+		"node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-navigation|@react-navigation/.*|react-native-svg|react-native-reanimated|react-native-haptic-feedback|react-native-unistyles)/)",
 	],
 };
