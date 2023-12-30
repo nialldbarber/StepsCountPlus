39f76a35047ab605cfffabb70085f50e012ce99c
Niall Barber <niall.barber@olioex.com>
2023-12-30 10:11:25 +0000

    chore: refactor challengeTypes array and ChallengesState interface

--- app/screens/challenges/index.tsx
+++ app/screens/challenges/index.tsx
@@ -12,9 +12,9 @@ import { space } from "@/app/design-system/space";
 import { useActiveValue } from "@/app/hooks/useActiveValue";
 import { RootChallengesScreen } from "@/app/navigation/types";
 import {
-	ChallengeType,
-	challengeTypes,
-	useChallengesStore,
+  ChallengeType,
+  challengeTypes,
+  useChallengesStore,
 } from "@/app/store/challenges";
 import { useNavigation } from "@react-navigation/native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -25,182 +25,179 @@ import { createStyleSheet, useStyles } from "react-native-unistyles";
 type Props = NativeStackScreenProps<RootChallengesScreen, "ChallengesRoot">;
 
 export function ChallengesScreen({ route }: Props) {
-	const { styles, theme } = useStyles(stylesheet);
-	const f = route?.params?.currentFilter;
-	const filter = route?.params?.currentFilter ?? "All";
+  const { styles, theme } = useStyles(stylesheet);
+  const filter = route?.params?.currentFilter ?? "all";
 
-	// console.log(f, filter);
+  const [currentFilter, setCurrentFilter] = useState<ChallengeType>(
+    filter as ChallengeType
+  );
+  const [currentFilterScreen, setCurrentFilterScreen] = useState<
+    "in-progress" | "completed"
+  >("in-progress");
 
-	const [currentFilter, setCurrentFilter] = useState<ChallengeType>(filter);
-	const [currentFilterScreen, setCurrentFilterScreen] = useState<
-		"in-progress" | "completed"
-	>("in-progress");
+  const { navigate } = useNavigation();
+  const { challenges, setRemoveChallenge } = useChallengesStore();
+  const { value, handleActiveValue } = useActiveValue();
 
-	const { navigate } = useNavigation();
-	const { challenges, setRemoveChallenge } = useChallengesStore();
-	const { value, handleActiveValue } = useActiveValue();
+  const filterChallengesByCategory = useMemo(() => {
+    if (currentFilter.toLowerCase() === "all") {
+      return challenges;
+    }
 
-	const filterChallengesByCategory = useMemo(() => {
-		if (currentFilter.toLowerCase() === "all") {
-			return challenges;
-		}
+    const filteredResults = challenges.filter((item) => {
+      const category = item.category || item.id.split("-")[0];
+      return (
+        category.toLowerCase() ===
+        currentFilter.toLowerCase().replaceAll(" ", "-")
+      );
+    });
+    return filteredResults;
+  }, [challenges, currentFilter]);
 
-		const filteredResults = challenges.filter((item) => {
-			const category = item.category || item.id.split("-")[0];
-			return (
-				category.toLowerCase() ===
-				currentFilter.toLowerCase().replaceAll(" ", "-")
-			);
-		});
-		return filteredResults;
-	}, [challenges, currentFilter]);
+  return (
+    <Layout>
+      <Box flex={1}>
+        <Bleed left="-20px" right="-20px" style={styles.bleed}>
+          <Row
+            marginHorizontal="20px"
+            marginBottom="10px"
+            gutter="6px"
+            a11yRole="tablist"
+            scroll
+          >
+            {challengeTypes.map(({ id, type }, index) => {
+              return (
+                <Chip
+                  key={id}
+                  label={type}
+                  onPress={() => {
+                    handleActiveValue(index);
+                    setCurrentFilter(type as ChallengeType);
+                  }}
+                  a11yLabel="test"
+                  a11yRole="menu"
+                  hitSlop={hitSlopLarge}
+                  isSelected={index === value}
+                  size="16px"
+                  height="36px"
+                />
+              );
+            })}
+          </Row>
+        </Bleed>
 
-	// console.log(JSON.stringify(challenges, null, 2));
+        <Bleed left="-20px" right="-20px" style={styles.bleed}>
+          <Row marginHorizontal="15px" a11yRole="tablist" scroll>
+            <Box flexDirection="row" marginBottom="20px">
+              <Pressable onPress={() => setCurrentFilterScreen("in-progress")}>
+                <Box
+                  backgroundColor="blackTwo"
+                  paddingVertical="5px"
+                  paddingHorizontal="15px"
+                  borderRadius="full"
+                  marginHorizontal="5px"
+                  borderWidth={1}
+                  styles={styles.currentFilterScreen(
+                    currentFilterScreen === "in-progress"
+                  )}
+                >
+                  <Text>In Progress</Text>
+                </Box>
+              </Pressable>
+              <Pressable onPress={() => setCurrentFilterScreen("completed")}>
+                <Box
+                  backgroundColor="blackTwo"
+                  paddingVertical="5px"
+                  paddingHorizontal="15px"
+                  borderRadius="full"
+                  marginHorizontal="5px"
+                  borderWidth={1}
+                  styles={styles.currentFilterScreen(
+                    currentFilterScreen === "completed"
+                  )}
+                >
+                  <Text>Completed</Text>
+                </Box>
+              </Pressable>
+              <Pressable onPress={() => navigate("SelectChallenge")}>
+                <Box
+                  backgroundColor="blackTwo"
+                  paddingVertical="5px"
+                  paddingHorizontal="15px"
+                  borderRadius="full"
+                  marginHorizontal="5px"
+                  borderWidth={1}
+                >
+                  <Text>Add New</Text>
+                </Box>
+              </Pressable>
+            </Box>
+          </Row>
+        </Bleed>
 
-	return (
-		<Layout>
-			<Box flex={1}>
-				<Bleed left="-20px" right="-20px" style={styles.bleed}>
-					<Row
-						marginHorizontal="20px"
-						marginBottom="10px"
-						gutter="6px"
-						a11yRole="tablist"
-						scroll
-					>
-						{challengeTypes.map(({ id, type }, index) => {
-							return (
-								<Chip
-									key={id}
-									label={type}
-									onPress={() => {
-										handleActiveValue(index);
-										setCurrentFilter(type);
-									}}
-									a11yLabel="test"
-									a11yRole="menu"
-									hitSlop={hitSlopLarge}
-									isSelected={index === value}
-									size="16px"
-									height="36px"
-								/>
-							);
-						})}
-					</Row>
-				</Bleed>
-
-				<Bleed left="-20px" right="-20px" style={styles.bleed}>
-					<Row marginHorizontal="15px" a11yRole="tablist" scroll>
-						<Box flexDirection="row" marginBottom="20px">
-							<Pressable onPress={() => setCurrentFilterScreen("in-progress")}>
-								<Box
-									backgroundColor="blackTwo"
-									paddingVertical="5px"
-									paddingHorizontal="15px"
-									borderRadius="full"
-									marginHorizontal="5px"
-									borderWidth={1}
-									styles={styles.currentFilterScreen(
-										currentFilterScreen === "in-progress",
-									)}
-								>
-									<Text>In Progress</Text>
-								</Box>
-							</Pressable>
-							<Pressable onPress={() => setCurrentFilterScreen("completed")}>
-								<Box
-									backgroundColor="blackTwo"
-									paddingVertical="5px"
-									paddingHorizontal="15px"
-									borderRadius="full"
-									marginHorizontal="5px"
-									borderWidth={1}
-									styles={styles.currentFilterScreen(
-										currentFilterScreen === "completed",
-									)}
-								>
-									<Text>Completed</Text>
-								</Box>
-							</Pressable>
-							<Pressable onPress={() => navigate("SelectChallenge")}>
-								<Box
-									backgroundColor="blackTwo"
-									paddingVertical="5px"
-									paddingHorizontal="15px"
-									borderRadius="full"
-									marginHorizontal="5px"
-									borderWidth={1}
-								>
-									<Text>Add New</Text>
-								</Box>
-							</Pressable>
-						</Box>
-					</Row>
-				</Bleed>
-
-				{currentFilterScreen === "completed" ? (
-					<Box>
-						<Text>COMPLETED</Text>
-					</Box>
-				) : challenges.length > 0 ? (
-					<Box>
-						<FlashList
-							data={filterChallengesByCategory}
-							estimatedItemSize={300}
-							renderItem={({ item }) => (
-								<Pressable
-									onPress={() =>
-										navigate("CurrentChallenge", { challenge: item })
-									}
-								>
-									<ChallengeCard
-										id={item.id}
-										key={item.id}
-										title={item.title}
-										difficulty={item.difficulty}
-										emoji={item.emoji}
-										isSet
-										fn={() => setRemoveChallenge(item.id)}
-										startDate={item.startDate}
-										target={item.target}
-										category={item.category}
-									/>
-								</Pressable>
-							)}
-						/>
-					</Box>
-				) : (
-					<>
-						<Box justifyContent="center" paddingVertical="20px">
-							<Text weight="bold">
-								Looks like you haven't started a challenge!
-							</Text>
-						</Box>
-						<Box
-							justifyContent="center"
-							alignItems="center"
-							paddingTop="20px"
-							paddingBottom="42px"
-						>
-							<Text level="heading" weight="bold" size="44px">
-								🙈
-							</Text>
-						</Box>
-					</>
-				)}
-			</Box>
-		</Layout>
-	);
+        {currentFilterScreen === "completed" ? (
+          <Box>
+            <Text>COMPLETED</Text>
+          </Box>
+        ) : challenges.length > 0 ? (
+          <Box>
+            <FlashList
+              data={filterChallengesByCategory}
+              estimatedItemSize={300}
+              renderItem={({ item }) => (
+                <Pressable
+                  onPress={() =>
+                    navigate("CurrentChallenge", { challenge: item })
+                  }
+                >
+                  <ChallengeCard
+                    id={item.id}
+                    key={item.id}
+                    title={item.title}
+                    difficulty={item.difficulty}
+                    emoji={item.emoji}
+                    isSet
+                    fn={() => setRemoveChallenge(item.id)}
+                    startDate={item.startDate}
+                    target={item.target}
+                    category={item.category}
+                  />
+                </Pressable>
+              )}
+            />
+          </Box>
+        ) : (
+          <>
+            <Box justifyContent="center" paddingVertical="20px">
+              <Text weight="bold">
+                Looks like you haven't started a challenge!
+              </Text>
+            </Box>
+            <Box
+              justifyContent="center"
+              alignItems="center"
+              paddingTop="20px"
+              paddingBottom="42px"
+            >
+              <Text level="heading" weight="bold" size="44px">
+                🙈
+              </Text>
+            </Box>
+          </>
+        )}
+      </Box>
+    </Layout>
+  );
 }
 
 const stylesheet = createStyleSheet(() => ({
-	bleed: {
-		marginBottom: space["15px"],
-	},
-	text: {
-		marginTop: space["6px"],
-	},
-	currentFilterScreen: (isActive) => ({
-		borderColor: isActive ? colors.primary : colors.blackTwo,
-	}),
+  bleed: {
+    marginBottom: space["15px"],
+  },
+  text: {
+    marginTop: space["6px"],
+  },
+  currentFilterScreen: (isActive) => ({
+    borderColor: isActive ? colors.primary : colors.blackTwo,
+  }),
 }));
--- app/store/challenges.ts
+++ app/store/challenges.ts
@@ -4,62 +4,64 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const challengeTypes = [
-	{ id: `${0}-challengeTypes`, type: "All" },
-	{ id: `${1}-challengeTypes`, type: "Steps" },
-	{ id: `${2}-challengeTypes`, type: "Distance" },
-	{ id: `${3}-challengeTypes`, type: "Flights" },
-	{ id: `${4}-challengeTypes`, type: "Long distance" },
-	{ id: `${5}-challengeTypes`, type: "F1 tracks" },
+  { id: `${0}-challengeTypes`, type: "All" },
+  { id: `${1}-challengeTypes`, type: "Steps" },
+  { id: `${2}-challengeTypes`, type: "Distance" },
+  { id: `${3}-challengeTypes`, type: "Flights" },
+  { id: `${4}-challengeTypes`, type: "Long distance" },
+  { id: `${5}-challengeTypes`, type: "F1 tracks" },
 ] as const;
+
 type Item = (typeof challengeTypes)[number];
+
 export type Value = Item["type"];
 type FormatValue<T extends string> = T extends `${infer Word} ${infer Rest}`
-	? `${Lowercase<Word>}-${FormatValue<Rest>}`
-	: Lowercase<T>;
+  ? `${Lowercase<Word>}-${FormatValue<Rest>}`
+  : Lowercase<T>;
 
 export type ChallengeType = FormatValue<Value>;
 
 export type Challenge = {
-	id: string;
-	title: string;
-	difficulty: "easy" | "medium" | "hard";
-	emoji: string;
-	startDate?: string;
-	target: number;
-	category: ChallengeType;
+  id: string;
+  title: string;
+  difficulty: "easy" | "medium" | "hard";
+  emoji: string;
+  startDate?: string;
+  target: number;
+  category: ChallengeType;
 };
 
 type ChallengesState = {
-	challenges: Array<Challenge>;
-	completedChallenges: Array<Challenge>;
+  challenges: Array<Challenge>;
+  completedChallenges: Array<Challenge>;
 };
 
 type ChallengesActions = {
-	setAddChallenge: (challenge: Challenge) => void;
-	setRemoveChallenge: (id: string) => void;
+  setAddChallenge: (challenge: Challenge) => void;
+  setRemoveChallenge: (id: string) => void;
 };
 
 export const useChallengesStore = create(
-	persist(
-		immer<ChallengesState & ChallengesActions>((set, get) => ({
-			challenges: [],
-			completedChallenges: [],
-			setAddChallenge: (challenge: Challenge) => {
-				set((state) => {
-					state.challenges.push(challenge);
-				});
-			},
-			setRemoveChallenge: (id: string) => {
-				set((state) => {
-					state.challenges = state.challenges.filter(
-						(challenge) => challenge.id !== id,
-					);
-				});
-			},
-		})),
-		{
-			name: "challenges_state",
-			storage: createJSONStorage(() => zustandStorage),
-		},
-	),
+  persist(
+    immer<ChallengesState & ChallengesActions>((set, get) => ({
+      challenges: [],
+      completedChallenges: [],
+      setAddChallenge: (challenge: Challenge) => {
+        set((state) => {
+          state.challenges.push(challenge);
+        });
+      },
+      setRemoveChallenge: (id: string) => {
+        set((state) => {
+          state.challenges = state.challenges.filter(
+            (challenge) => challenge.id !== id
+          );
+        });
+      },
+    })),
+    {
+      name: "challenges_state",
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
 );
