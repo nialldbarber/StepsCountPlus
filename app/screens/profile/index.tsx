4933bf7a9944da6e82cde3e949e589849fd644c8
Niall Barber <niall.barber@olioex.com>
2023-12-31 11:19:43 +0000

    chore: add profile screen content

--- app/screens/profile/index.tsx
+++ app/screens/profile/index.tsx
@@ -1,10 +1,28 @@
 import { ScreenHeader } from "@/app/components/screen-header";
+import { Box } from "@/app/design-system/components/box";
 import { Layout } from "@/app/design-system/components/layout";
+import { Text } from "@/app/design-system/components/text";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 export function ProfileScreen() {
-	return (
-		<Layout>
-			<ScreenHeader title="Profile" />
-		</Layout>
-	);
+  const { styles } = useStyles(stylesheet);
+
+  return (
+    <Layout>
+      <ScreenHeader title="Profile" />
+      <Box styles={styles.container}>
+        <Text>
+          Become a member to get access to all the features of the app.
+        </Text>
+      </Box>
+    </Layout>
+  );
 }
+
+const stylesheet = createStyleSheet(() => ({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
