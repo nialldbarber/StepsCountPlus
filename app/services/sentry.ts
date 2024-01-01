2d436cfb9889728fbfc72608aa0e60553016708d
Niall Barber <niall.barber@olioex.com>
2024-01-01 10:08:31 +0000

    chore: update Sentry configuration

--- app/services/sentry.ts
+++ app/services/sentry.ts
@@ -2,6 +2,10 @@ import * as Sentry from "@sentry/react-native";
 import Config from "react-native-config";
 
 Sentry.init({
-	dsn: Config.SENTRY_DSN,
-	tracesSampleRate: __DEV__ ? 1.0 : 0.5,
+  dsn: Config.SENTRY_DSN,
+  // tracesSampleRate: __DEV__ ? 1.0 : 0.5,
+  tracesSampleRate: 1.0,
+  _experiments: {
+    profilesSampleRate: 1.0,
+  },
 });
