a6178aaaac24cc85428d41a6b2cad46f52e1b71c
Niall Barber <niall.barber@olioex.com>
2024-01-02 07:04:18 +0000

    chore: refactor SelectChallengeScreen component

--- app/screens/select-challenge/index.tsx
+++ app/screens/select-challenge/index.tsx
@@ -4,16 +4,16 @@ import { Card } from "@/app/design-system/components/card";
 import { Layout } from "@/app/design-system/components/layout";
 
 export function SelectChallengeScreen() {
-	return (
-		<Layout>
-			<ScreenHeader title="Choose a category" />
-			<Box gap="20px" justifyContent="center" marginTop="20px">
-				<Card challengeType="steps" />
-				<Card challengeType="distance" />
-				<Card challengeType="flights" />
-				<Card challengeType="long-distance" />
-				<Card challengeType="f1-tracks" />
-			</Box>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <ScreenHeader title="Choose a category" />
+      <Box gap="20px" justifyContent="center" marginVertical="20px">
+        <Card challengeType="steps" />
+        <Card challengeType="distance" />
+        <Card challengeType="flights" />
+        <Card challengeType="long-distance" />
+        <Card challengeType="f1-tracks" />
+      </Box>
+    </Layout>
+  );
 }
