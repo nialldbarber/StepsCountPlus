09a2706bf8b320f3e962192f8d63bb3418078fe6
Niall Barber <niall.barber@olioex.com>
2024-01-03 09:34:30 +0000

    chore: refactor Pressable component to add forceHaptic prop

--- app/design-system/components/pressable.tsx
+++ app/design-system/components/pressable.tsx
@@ -4,40 +4,47 @@ import { Pressable as NativePressable } from "react-native";
 import RNReactNativeHapticFeedback from "react-native-haptic-feedback";
 
 export interface PressableProps extends NativePressableProps, A11y {
-	onPress?: (...args: unknown[]) => unknown;
-	haptics?: {
-		type: "action" | "notification";
-		level?: "Success" | "Warning" | "Error" | "Light" | "Medium" | "Heavy";
-	};
+  onPress?: (...args: unknown[]) => unknown;
+  haptics?: {
+    type: "action" | "notification";
+    level?: "Success" | "Warning" | "Error" | "Light" | "Medium" | "Heavy";
+  };
+  forceHaptic?: boolean;
 }
 
 export function Pressable({
-	onPress,
-	haptics = { type: "action" },
-	a11yLabel,
-	a11yHint,
-	a11yRole = "button",
-	a11yState,
-	children,
-	...rest
+  onPress,
+  haptics = { type: "action" },
+  forceHaptic = false,
+  a11yLabel,
+  a11yHint,
+  a11yRole = "button",
+  a11yState,
+  children,
+  ...rest
 }: PressableProps) {
-	const handleOnPress = () => {
-		if (onPress === null || onPress === undefined) return;
-		RNReactNativeHapticFeedback.trigger("impactMedium");
-		onPress();
-	};
+  const handleOnPress = () => {
+    if (forceHaptic) {
+      RNReactNativeHapticFeedback.trigger("impactMedium");
+      return;
+    }
 
-	return (
-		<NativePressable
-			onPress={handleOnPress}
-			{...rest}
-			accessible
-			accessibilityLabel={a11yLabel}
-			accessibilityHint={a11yHint}
-			accessibilityRole={a11yRole}
-			accessibilityState={a11yState}
-		>
-			{children}
-		</NativePressable>
-	);
+    if (onPress === null || onPress === undefined) return;
+    RNReactNativeHapticFeedback.trigger("impactMedium");
+    onPress();
+  };
+
+  return (
+    <NativePressable
+      onPress={handleOnPress}
+      {...rest}
+      accessible
+      accessibilityLabel={a11yLabel}
+      accessibilityHint={a11yHint}
+      accessibilityRole={a11yRole}
+      accessibilityState={a11yState}
+    >
+      {children}
+    </NativePressable>
+  );
 }
