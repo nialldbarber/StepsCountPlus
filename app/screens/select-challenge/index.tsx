b15c2d133dd3ddae313535780f806dc80e7d19e5
Niall Barber <niall.barber@olioex.com>
2024-01-03 16:11:44 +0000

    chore: add text for challenge category selection

--- app/screens/select-challenge/index.tsx
+++ app/screens/select-challenge/index.tsx
@@ -3,11 +3,17 @@ import { Bleed } from "@/app/design-system/components/bleed";
 import { Box } from "@/app/design-system/components/box";
 import { Card } from "@/app/design-system/components/card";
 import { Layout } from "@/app/design-system/components/layout";
+import { Text } from "@/app/design-system/components/text";
 
 export function SelectChallengeScreen() {
   return (
     <Layout>
       <ScreenHeader title="Choose a category" />
+      <Box paddingTop="20px" alignItems="center">
+        <Text size="12px">
+          (You may only choose one challenge per category!)
+        </Text>
+      </Box>
       <Bleed left="-10px" right="-10px">
         <Box flexDirection="row" flexWrap="wrap" marginVertical="20px">
           <Card challengeType="steps" />
