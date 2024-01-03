14e9c8a6d9cb8a0bbd8c7b754006a8ad3640aec1
Niall Barber <niall.barber@olioex.com>
2024-01-03 07:41:30 +0000

    chore: refactor GoalsScreen component

--- app/screens/goals/index.tsx
+++ app/screens/goals/index.tsx
@@ -3,19 +3,19 @@ import { DailyFlightsGoal } from "@/app/components/goals/flights";
 import { DailyStepsGoal } from "@/app/components/goals/steps";
 import { Layout } from "@/app/design-system/components/layout";
 import { Stack } from "@/app/design-system/components/stack";
-import { Text } from "@/app/design-system/components/text";
 
 export function GoalsScreen() {
-	return (
-		<Layout>
-			<Stack gutter="10px">
-				<Text level="heading" withEmoji textStyles={{ lineHeight: 23 }}>
+  return (
+    <Layout>
+      <Stack gutter="10px">
+        {/* TODO move this into an info box */}
+        {/* <Text level="heading" withEmoji textStyles={{ lineHeight: 23 }}>
 					Set your steps, flights, and distance goals and track your progress 💪
-				</Text>
-				<DailyStepsGoal />
-				<DailyFlightsGoal />
-				<DailyDistanceGoal />
-			</Stack>
-		</Layout>
-	);
+				</Text> */}
+        <DailyStepsGoal />
+        <DailyFlightsGoal />
+        <DailyDistanceGoal />
+      </Stack>
+    </Layout>
+  );
 }
