d9b54d3f594ffafc6b3cb9bdd01aded05dff2fd2
Niall Barber <niall.barber@olioex.com>
2024-01-04 09:28:43 +0000

    chore: add Bleed and Card components to CreateYourChallengeScreen

--- app/screens/create-your-challenge/index.tsx
+++ app/screens/create-your-challenge/index.tsx
@@ -1,15 +1,20 @@
 import { ScreenHeader } from "@/app/components/screen-header";
+import { Bleed } from "@/app/design-system/components/bleed";
 import { Box } from "@/app/design-system/components/box";
+import { Card } from "@/app/design-system/components/card";
 import { Layout } from "@/app/design-system/components/layout";
-import { Text } from "@/app/design-system/components/text";
 
 export function CreateYourChallengeScreen() {
   return (
     <Layout>
       <ScreenHeader title="Create your challenge" />
-      <Box paddingTop="20px" alignItems="center">
-        <Text size="12px">Hello there</Text>
-      </Box>
+      <Bleed left="-10px" right="-10px">
+        <Box flexDirection="row" flexWrap="wrap" marginVertical="20px">
+          <Card challengeType="custom-steps" />
+          <Card challengeType="custom-flights" />
+          <Card challengeType="custom-distance" />
+        </Box>
+      </Bleed>
     </Layout>
   );
 }
