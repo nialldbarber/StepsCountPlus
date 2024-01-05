bba519dbc82e94a2b9ea471a1065ebae93c42330
Niall Barber <niall.barber@olioex.com>
2024-01-05 08:42:54 +0000

    chore: refactor formatNumber function to include decimal option

--- app/lib/format/numbers.ts
+++ app/lib/format/numbers.ts
@@ -1,10 +1,10 @@
-export function formatNumber(num: number | string) {
-	if (typeof num === "string") {
-		return Intl.NumberFormat().format(Number(num));
-	}
-	return Intl.NumberFormat().format(num);
+export function formatNumber(num: number | string, decimals = false) {
+  const formattedNumber = typeof num === "string" ? Number(num) : num;
+  return decimals
+    ? Intl.NumberFormat().format(formattedNumber)
+    : Intl.NumberFormat().format(parseInt(formattedNumber.toString()));
 }
 
 export function determinePercentage(percentage: number, target: number) {
-	return ((percentage / target) * 100).toFixed(0);
+  return ((percentage / target) * 100).toFixed(0);
 }
