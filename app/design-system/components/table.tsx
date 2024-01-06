e4c71f400c07f46baa1126e1c9e18b1442864527
Niall Barber <niall.barber@olioex.com>
2024-01-06 10:21:00 +0000

    chore: refactor table component styling

--- app/design-system/components/table.tsx
+++ app/design-system/components/table.tsx
@@ -4,24 +4,24 @@ import flattenChildren from "react-keyed-flatten-children";
 import { useStyles } from "react-native-unistyles";
 
 export function Table({ children }: PropsWithChildren) {
-	const { theme } = useStyles();
-	const flattenedChildren = flattenChildren(children);
+  const { theme } = useStyles();
+  const flattenedChildren = flattenChildren(children);
 
-	const rowStyles = [
-		{ backgroundColor: theme.colors.tableEvenBackgroundColor }, // Style for even rows
-		{ backgroundColor: theme.colors.tableOddBackgroundColor }, // Style for odd rows
-	];
+  const rowStyles = [
+    { backgroundColor: theme.colors.tableEvenBackgroundColor }, // for even rows
+    { backgroundColor: theme.colors.tableOddBackgroundColor }, // for odd rows
+  ];
 
-	const styledChildren = flattenedChildren.map((child, index) => (
-		<Box
-			key={index}
-			styles={rowStyles[index % rowStyles.length]}
-			borderRadius="medium"
-			padding="10px"
-		>
-			{child}
-		</Box>
-	));
+  const styledChildren = flattenedChildren.map((child, index) => (
+    <Box
+      key={`table-row-${index}`}
+      styles={rowStyles[index % rowStyles.length]}
+      borderRadius="medium"
+      padding="10px"
+    >
+      {child}
+    </Box>
+  ));
 
-	return <Box>{styledChildren}</Box>;
+  return <Box>{styledChildren}</Box>;
 }
