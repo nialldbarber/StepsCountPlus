13a2d4c063579522e50dc13660916535660f232f
Niall Barber <niall.barber@olioex.com>
2024-01-06 10:19:48 +0000

    chore: refactor Stack component to improve readability and add documentation

--- app/design-system/components/stack.tsx
+++ app/design-system/components/stack.tsx
@@ -6,32 +6,40 @@ import flattenChildren from "react-keyed-flatten-children";
 import type { ViewProps } from "react-native";
 
 interface StackProps extends ViewProps {
-	margin?: Space;
-	gutter?: Space;
+  /**
+   * The amount of space around all
+   * child elements
+   */
+  margin?: Space;
+  /**
+   * The amount of space between each
+   * child element
+   */
+  gutter?: Space;
 }
 
 export function Stack({
-	margin = "0px",
-	gutter = "0px",
-	children: childProp,
+  margin = "0px",
+  gutter = "0px",
+  children: childProp,
 }: PropsWithChildren<StackProps>) {
-	const children = flattenChildren(childProp);
+  const children = flattenChildren(childProp);
 
-	return (
-		<Box margin={margin}>
-			{Children.map(children, (child, index) => {
-				const first = index === 0;
-				const last = index === children.length - 1;
+  return (
+    <Box margin={margin}>
+      {Children.map(children, (child, index) => {
+        const first = index === 0;
+        const last = index === children.length - 1;
 
-				return (
-					<Box
-						paddingTop={first ? "0px" : gutter}
-						paddingBottom={last ? "0px" : gutter}
-					>
-						{child}
-					</Box>
-				);
-			})}
-		</Box>
-	);
+        return (
+          <Box
+            paddingTop={first ? "0px" : gutter}
+            paddingBottom={last ? "0px" : gutter}
+          >
+            {child}
+          </Box>
+        );
+      })}
+    </Box>
+  );
 }
