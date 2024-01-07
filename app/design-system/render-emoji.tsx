2006ff9cb733a81f86085511c2318c71fdc7adcd
Niall Barber <niall.barber@olioex.com>
2024-01-07 19:13:18 +0000

    chore: moved styles to unistyles for consistency

--- app/design-system/render-emoji.tsx
+++ app/design-system/render-emoji.tsx
@@ -1,15 +1,10 @@
 import createEmojiRegex from "emoji-regex";
 import type { ReactNode } from "react";
 import React, { Fragment } from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 const emojiRegex = createEmojiRegex();
-const styles = StyleSheet.create({
-  emoji: {
-    color: "black",
-    fontFamily: "System",
-  },
-});
 
 /**
  * Without this, the `line-height` goes off when
@@ -18,26 +13,34 @@ const styles = StyleSheet.create({
  * @returns React.JSX.Element
  */
 export function renderStringWithEmoji(stringNode: ReactNode) {
-  const strings = Array.isArray(stringNode) ? stringNode : [stringNode];
-  return (
-    <Fragment>
-      {strings.map((string) => {
-        if (typeof string !== "string") {
-          return string;
-        }
+	const { styles } = useStyles(stylesheet);
+	const strings = Array.isArray(stringNode) ? stringNode : [stringNode];
+	return (
+		<Fragment>
+			{strings.map((string) => {
+				if (typeof string !== "string") {
+					return string;
+				}
 
-        const emojis = string.match(emojiRegex);
-        if (emojis === null) return string;
+				const emojis = string.match(emojiRegex);
+				if (emojis === null) return string;
 
-        return string.split(emojiRegex).map((stringPart, index) => (
-          <Fragment key={`emoji-${index}`}>
-            {stringPart}
-            {emojis[index] ? (
-              <Text style={styles.emoji}>{emojis[index]}</Text>
-            ) : null}
-          </Fragment>
-        ));
-      })}
-    </Fragment>
-  );
+				return string.split(emojiRegex).map((stringPart, index) => (
+					<Fragment key={`emoji-${index}`}>
+						{stringPart}
+						{emojis[index] ? (
+							<Text style={styles.emoji}>{emojis[index]}</Text>
+						) : null}
+					</Fragment>
+				));
+			})}
+		</Fragment>
+	);
 }
+
+const stylesheet = createStyleSheet(() => ({
+	emoji: {
+		color: "black",
+		fontFamily: "System",
+	},
+}));
