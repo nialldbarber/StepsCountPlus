47e79df121deabe0bc923d5e3e46aad99a2fe5d8
Niall Barber <niall.barber@olioex.com>
2024-01-07 20:09:58 +0000

    chore: refactor shadow.ts to improve default setup

--- app/design-system/shadow.ts
+++ app/design-system/shadow.ts
@@ -1,24 +1,26 @@
 import { colors } from "@/app/design-system/colors";
 
+// @TODO: this default set up SUCKS
+// look into how SwiftUI defaults work to try and match that
 export const shadow = ({
-  width = 0,
-  height = 1,
-  opacity = 0.08,
-  radius = 5,
-  elevation = 0.1,
+	width = 0,
+	height = 1,
+	opacity = 0.08,
+	radius = 5,
+	elevation = 0.1,
 }: {
-  width?: number;
-  height?: number;
-  opacity?: number;
-  radius?: number;
-  elevation?: number;
+	width?: number;
+	height?: number;
+	opacity?: number;
+	radius?: number;
+	elevation?: number;
 } = {}) => ({
-  shadowColor: colors.blackTwo,
-  shadowOffset: {
-    width,
-    height,
-  },
-  shadowOpacity: opacity,
-  shadowRadius: radius,
-  elevation,
+	shadowColor: colors.blackTwo,
+	shadowOffset: {
+		width,
+		height,
+	},
+	shadowOpacity: opacity,
+	shadowRadius: radius,
+	elevation,
 });
